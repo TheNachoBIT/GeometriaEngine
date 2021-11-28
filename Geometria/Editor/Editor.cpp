87b11d57269725ecd1c4c5e1fa8ef473d707e207
TheNachoBIT <nacholopezcorvalan2@gmail.com>
2021-11-28 18:45:20 -0300

    Update Editor.cpp

--- Geometria/Editor/Editor.cpp
+++ Geometria/Editor/Editor.cpp
@@ -270,8 +270,9 @@ void Editor::SwitchEditorView()
 	{
 		Editor::EditorViewButton->colorRef = new Color(1, 0.9, 0);
 		Editor::GameViewButton->colorRef = new Color(Color::white());
-		Graphics::MainCamera()->editorModeCamera = true;
 	}
+
+	Graphics::MainCamera()->editorModeCamera = true;
 }
 
 void Editor::SwitchGameView()
@@ -280,8 +281,9 @@ void Editor::SwitchGameView()
 	{
 		Editor::EditorViewButton->colorRef = new Color(Color::white());
 		Editor::GameViewButton->colorRef = new Color(1, 0.9, 0);
-		Graphics::MainCamera()->editorModeCamera = false;
 	}
+
+	Graphics::MainCamera()->editorModeCamera = false;
 }
 
 void Editor::MainMenuBarBegin()
